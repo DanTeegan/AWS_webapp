ea698632507afe69282e96728753aeeeb993d8f3
dan. teegan <dan.teegan@hotmail.com>
2020-08-07 22:19:08 +0100

    Port 80 working

--- app/provisions.sh
+++ app/provisions.sh
@@ -16,8 +16,9 @@ sudo touch reverse-proxy.conf
 sudo chmod 666 reverse-proxy.conf
 echo "server{
   listen 80;
+  server_name development.local;
   location / {
-      proxy_pass http://192.168.10.100:3000;
+      proxy_pass http://127.0.0.1:3000;
   }
 }" >> reverse-proxy.conf
 sudo ln -s /etc/nginx/sites-available/reverse-proxy.conf /etc/nginx/sites-enabled/reverse-proxy.conf
@@ -33,17 +34,3 @@ sudo apt-get install nodejs -y
 
 # install pm2
 sudo npm install pm2 -g
-
-
-# App set up
-export DB_HOST="mongodb://192.168.10.150:27017/posts"
-cd /home/ubuntu/app
-sudo su
-npm install
-# node app.js
-
-
-
-
-
-
