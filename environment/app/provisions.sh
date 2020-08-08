0389a49608d05f7edd7c57ea8e06dfd5c6f2ec27
dan. teegan <dan.teegan@hotmail.com>
2020-08-08 21:59:00 +0100

    Creating db folder with provisions

--- environment/app/provisions.sh
+++ environment/app/provisions.sh
@@ -41,7 +41,7 @@ sudo npm install pm2 -g
 
 
 # App set up
-export DB_HOST="mongodb://192.168.10.150:27017/posts"
+export DB_HOST="mongodb://52.208.188.187:27017/posts"
 cd /home/ubuntu/app
 sudo su
 npm install
